b6149d8a2329b8ada4ca25c602855f018947d272
Jordan S <jpstanway@gmail.com>
2021-07-05 15:54:04 -0700

    convert times to decimal

--- time_calculator.py
+++ time_calculator.py
@@ -1,3 +1,19 @@
+def convertToDecimal(time):
+    conv = time.replace(":", ".")
+    return float(conv)
+
+
 def add_time(start, duration):
+    new_time = ""
+
+    # separate AM/PM
+    strTime = start.split(" ")
+    durTime = duration.split(" ")
+    print(durTime)
+
+    # convert time to minutes
+    strDec = convertToDecimal(strTime[0])
+    durDec = convertToDecimal(durTime[0])
+    print(strDec, durDec)
 
     return new_time
