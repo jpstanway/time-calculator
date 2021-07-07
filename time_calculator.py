7e5015ae6e358d95d7fcadbc0e7cad9b1d84c387
Jordan S <jpstanway@gmail.com>
2021-07-06 21:29:59 -0700

    add up hours

--- time_calculator.py
+++ time_calculator.py
@@ -1,3 +1,11 @@
+import math
+
+
+def getHours(time):
+    split = time.split(":")
+    return int(split[0])
+
+
 def convertToDecimal(time):
     conv = time.replace(":", ".")
     return float(conv)
@@ -5,15 +13,33 @@ def convertToDecimal(time):
 
 def add_time(start, duration):
     new_time = ""
+    new_hours = 0
 
     # separate AM/PM
     strTime = start.split(" ")
     durTime = duration.split(" ")
-    print(durTime)
 
     # convert time to minutes
     strDec = convertToDecimal(strTime[0])
     durDec = convertToDecimal(durTime[0])
-    print(strDec, durDec)
+
+    strHours = getHours(strTime[0])
+    durHours = getHours(durTime[0])
+
+    if (durHours >= 12):
+        rotations = math.floor(durHours / 12)
+        leftOver = durHours % 12
+        isEven = (leftOver % 2) == 0
+
+        # if (isEven == False):
+
+        new_hours = strHours + leftOver
+    else:
+        new_hours = strHours + durHours
+
+    if (new_hours > 12):
+        new_hours -= 12
+
+    new_time += str(new_hours)
 
     return new_time
